576942f92b0af8dfa395c584cc2a081099971c34
israj haliri <israj.haliri@gmail.com>
2019-05-16 11:18:22 +0700

    add list to api

---
+++ swift-integration/CommonModel.swift
@@ -0,0 +1,13 @@
+//
+//  CommonModel.swift
+//  swift-integration
+//
+//  Created by IsrajHaliri on 16/05/19.
+//  Copyright © 2019 IsrajHaliri. All rights reserved.
+//
+
+import UIKit
+
+class CommonModel: NSObject {
+
+}
